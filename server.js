3811eceebed604b621d31fb2bd49d4e99b8cfc8a
CaveManEN <ianccody@gmail.com>
2024-02-23 13:17:12 -0600

    changed the route to routes in the server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes'); // Import the htmlRoutes module
+const htmlRoutes = require('./server/routes/htmlRoutes'); // Import the htmlRoutes module
 
 const app = express();
 const PORT = process.env.PORT || 3000;
